dd972f2d57955c329345399978d976f24cd3b58b
Usman-Marwat <usk895907a2@gmail.com>
2023-12-11 13:40:00 +0500

    show plants data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,77 @@
 import { StyleSheet } from 'react-native';
 
-import { Text, View } from '../../components/Themed';
+import { View } from '../../components/Themed';
+import Animated, { FadeIn } from 'react-native-reanimated';
+import { useState } from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+import { Avatar, Button, Card, Text } from 'react-native-paper';
+
+import plants from '../../data/plants';
+import { ph, pw } from '../../constants/Dimensions';
+import { Image } from 'expo-image';
 
 export default function TabOneScreen() {
+	const [data, setData] = useState(plants);
+
+	const LeftContent = (props: any) => (
+		<Avatar.Icon
+			{...props}
+			icon="flower"
+			theme={{ colors: { primary: 'green' } }}
+		/>
+	);
+
 	return (
-		<View style={styles.container}>
-			<Text>Hello World</Text>
-		</View>
+		<Animated.View style={styles.container} entering={FadeIn.duration(1000)}>
+			<FlatList
+				data={data}
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{ paddingVertical: ph(2) }}
+				ItemSeparatorComponent={() => (
+					<View style={{ marginVertical: ph(1) }} />
+				)}
+				renderItem={({ item: p }) => (
+					<Card style={styles.card}>
+						<Card.Title
+							title={p.name}
+							subtitle={p.species}
+							left={LeftContent}
+						/>
+						{/* <Card.Cover style={styles.cover} source={{ uri: p.image }} /> */}
+						<Image
+							source={{ uri: p.image }}
+							style={{ width: '100%', height: ph(30) }}
+						/>
+						<Card.Content>
+							<Text variant="bodyMedium">{p.description}</Text>
+						</Card.Content>
+						<Card.Actions>
+							<Button
+								icon="details"
+								buttonColor="green"
+								textColor="white"
+								style={{ borderWidth: 0 }}
+							>
+								Details
+							</Button>
+						</Card.Actions>
+					</Card>
+				)}
+				keyExtractor={(item) => item.name}
+			/>
+		</Animated.View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
+	},
+	card: {
+		borderRadius: 10,
+		marginHorizontal: pw(5),
+	},
+	cover: {
+		borderRadius: 0,
 	},
 });
---
+++ data/plants.ts
@@ -0,0 +1,205 @@
+export default [
+	{
+		name: 'Rose',
+		species: 'Rosa',
+		image: 'https://source.unsplash.com/featured/?rose',
+		description:
+			'Roses are perennial flowering plants known for their beauty and fragrance. They come in a variety of colors and are often used as symbols of love and romance.',
+	},
+	{
+		name: 'Sunflower',
+		species: 'Helianthus annuus',
+		image: 'https://source.unsplash.com/featured/?sunflower',
+		description:
+			'Sunflowers are tall, bright yellow flowers that follow the sun throughout the day. They are native to North America and are known for their cheerful appearance in gardens.',
+	},
+	{
+		name: 'Fern',
+		species: 'Polypodiopsida',
+		image: 'https://source.unsplash.com/featured/?fern',
+		description:
+			'Ferns are non-flowering vascular plants with feathery leaves. They thrive in shady and moist environments, adding a touch of greenery to woodland areas.',
+	},
+	{
+		name: 'Tulip',
+		species: 'Tulipa',
+		image: 'https://source.unsplash.com/featured/?tulip',
+		description:
+			'Tulips are bulbous spring-blooming flowers that come in various colors. They are associated with beauty, elegance, and the arrival of spring.',
+	},
+	{
+		name: 'Lavender',
+		species: 'Lavandula',
+		image: 'https://source.unsplash.com/featured/?lavender',
+		description:
+			'Lavender is a fragrant herb known for its purple flowers and soothing aroma. It is often used in aromatherapy, culinary dishes, and as an ornamental plant in gardens.',
+	},
+	{
+		name: 'Daisy',
+		species: 'Bellis perennis',
+		image: 'https://source.unsplash.com/featured/?daisy',
+		description:
+			'Daisies are charming, white flowers with yellow centers. They symbolize purity and innocence and are commonly found in meadows and gardens.',
+	},
+	{
+		name: 'Orchid',
+		species: 'Orchidaceae',
+		image: 'https://source.unsplash.com/featured/?orchid',
+		description:
+			'Orchids are exotic and diverse flowering plants, known for their intricate blooms. They are often associated with beauty, love, and luxury.',
+	},
+	{
+		name: 'Bamboo',
+		species: 'Bambusoideae',
+		image: 'https://source.unsplash.com/featured/?bamboo',
+		description:
+			'Bamboo is a versatile and fast-growing plant with a variety of uses. It is prized for its strength, flexibility, and contribution to sustainable living.',
+	},
+	{
+		name: 'Maple Tree',
+		species: 'Acer',
+		image: 'https://source.unsplash.com/featured/?maple-tree',
+		description:
+			'Maple trees are deciduous trees known for their vibrant autumn foliage. They are valued for their wood, syrup production, and their role in landscaping.',
+	},
+	{
+		name: 'Cactus',
+		species: 'Cactaceae',
+		image: 'https://source.unsplash.com/featured/?cactus',
+		description:
+			'Cacti are succulent plants adapted to arid environments. They come in various shapes and sizes, with unique adaptations for water conservation.',
+	},
+	{
+		name: 'Cherry Blossom',
+		species: 'Prunus serrulata',
+		image: 'https://source.unsplash.com/featured/?cherry-blossom',
+		description:
+			'Cherry blossoms are iconic spring flowers, celebrated for their delicate pink and white petals. They symbolize renewal, beauty, and the fleeting nature of life.',
+	},
+	{
+		name: 'Palm Tree',
+		species: 'Arecaceae',
+		image: 'https://source.unsplash.com/featured/?palm-tree',
+		description:
+			'Palm trees are tropical plants known for their distinctive fronds and association with sunny climates. They add a touch of paradise to landscapes.',
+	},
+	{
+		name: 'Hydrangea',
+		species: 'Hydrangea',
+		image: 'https://source.unsplash.com/featured/?hydrangea',
+		description:
+			'Hydrangeas are deciduous shrubs with large, showy flowerheads. They come in various colors and are popular in gardens and floral arrangements.',
+	},
+	{
+		name: 'Aloe Vera',
+		species: 'Aloe barbadensis miller',
+		image: 'https://source.unsplash.com/featured/?aloe-vera',
+		description:
+			'Aloe vera is a succulent known for its medicinal properties. The gel inside its leaves is used to soothe burns, moisturize skin, and promote healing.',
+	},
+	{
+		name: 'Poinsettia',
+		species: 'Euphorbia pulcherrima',
+		image: 'https://source.unsplash.com/featured/?poinsettia',
+		description:
+			'Poinsettias are vibrant red and green plants commonly associated with Christmas. They are popular as festive decorations during the holiday season.',
+	},
+	{
+		name: 'Magnolia',
+		species: 'Magnolia',
+		image: 'https://source.unsplash.com/featured/?magnolia',
+		description:
+			'Magnolias are elegant flowering trees or shrubs with large, fragrant blossoms. They are admired for their beauty and are often found in gardens and parks.',
+	},
+	{
+		name: 'Jasmine',
+		species: 'Jasminum',
+		image: 'https://source.unsplash.com/featured/?jasmine',
+		description:
+			'Jasmine is a fragrant flowering plant known for its aromatic white flowers. It is often used in perfumes, teas, and as an ornamental shrub.',
+	},
+	{
+		name: 'Fuchsia',
+		species: 'Fuchsia',
+		image: 'https://source.unsplash.com/featured/?fuchsia',
+		description:
+			'Fuchsias are colorful, pendulous flowers that attract hummingbirds. They come in various shades and are popular in hanging baskets and gardens.',
+	},
+	{
+		name: 'Carnation',
+		species: 'Dianthus caryophyllus',
+		image: 'https://source.unsplash.com/featured/?carnation',
+		description:
+			'Carnations are classic flowers with ruffled petals and a spicy fragrance. They are often used in bouquets, symbolizing love and admiration.',
+	},
+	{
+		name: 'Azalea',
+		species: 'Rhododendron',
+		image: 'https://source.unsplash.com/featured/?azalea',
+		description:
+			'Azaleas are flowering shrubs known for their vibrant and diverse colors. They thrive in acidic soils and are popular in gardens and landscapes.',
+	},
+	{
+		name: 'Bougainvillea',
+		species: 'Bougainvillea',
+		image: 'https://source.unsplash.com/featured/?bougainvillea',
+		description:
+			'Bougainvilleas are tropical and subtropical vines with vibrant, papery bracts surrounding small, inconspicuous flowers. They add a burst of color to gardens.',
+	},
+	{
+		name: 'Snapdragon',
+		species: 'Antirrhinum',
+		image: 'https://source.unsplash.com/featured/?snapdragon',
+		description:
+			"Snapdragons are tall, spiky flowers with a unique shape resembling a dragon's mouth. They are popular in floral arrangements and cottage gardens.",
+	},
+	{
+		name: 'African Violet',
+		species: 'Saintpaulia',
+		image: 'https://source.unsplash.com/featured/?african-violet',
+		description:
+			'African Violets are small, compact plants with fuzzy leaves and colorful, delicate flowers. They are commonly kept as houseplants and require gentle care.',
+	},
+	{
+		name: 'Daffodil',
+		species: 'Narcissus',
+		image: 'https://source.unsplash.com/featured/?daffodil',
+		description:
+			'Daffodils are trumpet-shaped flowers with bright yellow or white petals. They are harbingers of spring and symbolize renewal and the promise of new beginnings.',
+	},
+	{
+		name: 'Spider Plant',
+		species: 'Chlorophytum comosum',
+		image: 'https://source.unsplash.com/featured/?spider-plant',
+		description:
+			'Spider Plants are easy-to-care-for indoor plants with arching leaves and small offshoots resembling spiders. They are known for their air-purifying qualities.',
+	},
+	{
+		name: 'Snake Plant',
+		species: 'Sansevieria trifasciata',
+		image: 'https://source.unsplash.com/featured/?snake-plant',
+		description:
+			"Snake Plants, also known as Mother-in-law's Tongue, are hardy indoor plants with upright leaves. They are known for their air-purifying abilities and low maintenance.",
+	},
+	{
+		name: 'Columbine',
+		species: 'Aquilegia',
+		image: 'https://source.unsplash.com/featured/?columbine',
+		description:
+			'Columbines are perennial plants with distinctive spurred flowers. They come in various colors and are attractive to hummingbirds and butterflies.',
+	},
+	{
+		name: 'Morning Glory',
+		species: 'Ipomoea',
+		image: 'https://source.unsplash.com/featured/?morning-glory',
+		description:
+			'Morning Glories are climbing vines with trumpet-shaped flowers that bloom in the morning. They are known for their rapid growth and vibrant hues.',
+	},
+	{
+		name: 'Chrysanthemum',
+		species: 'Chrysanthemum',
+		image: 'https://source.unsplash.com/featured/?chrysanthemum',
+		description:
+			'Chrysanthemums, or mums, are fall-blooming flowers with a wide range of colors and shapes. They symbolize autumn and are popular in gardens and floral arrangements.',
+	},
+];
